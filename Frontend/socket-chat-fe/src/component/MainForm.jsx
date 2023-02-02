3409055ed5fc5021d20b0c1254f8296dbb0bb2ea
Adarsh Kumar <adarshk0802@gmail.com>
2023-02-02 21:44:30 +0530

    first commit

---
+++ Frontend/socket-chat-fe/src/component/MainForm.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+
+function MainForm() {
+
+    const navigate = useNavigate();
+    const [error,setError]=useState("")
+
+    const [data,setdata]= useState({name:"",room:""})
+
+const handleChange=(e)=>{
+console.log(e.target.name, e.target.value)
+setdata({
+    ...data,
+    [e.target.name]:e.target.value
+})}
+
+const validation =()=>{
+    if(!data.name){
+        setError("Please enter your name")
+        return false
+    }
+    if(!data.room){
+        setError("please enter room")
+        return false
+    }
+    setError("")
+    return true
+
+
+}
+
+
+const handleSubmit =(e)=>{
+    e.preventDefault()
+    const isValid=validation()
+    if(isValid){
+        navigate(`/chat/${data.room}`,{state:data})
+    }
+}
+
+
+
+  return (
+   <div className='px-3 py-4 shadow bg-white text-dark border rounded row'>
+    <form onSubmit={handleSubmit}>
+        <div className='form-group mb-4'>
+            <h2 className='text-warning mb-4'>Welcome to Chatclub</h2>
+        </div>
+        <div className='form-group mb-4'>
+            <input type="text" className='form-control bg-light' name='name' onChange={handleChange} placeholder='Enter Name'/>
+        </div>
+        <div className='form-group mb-4'>
+            <select className='form-select bg-light' onChange={handleChange} name="room">
+                <option value="">Select Room</option>
+                <option value="gaming">Gaming</option>
+                <option value="coding">Coding</option>
+                <option value="socialMedia">Social Media</option>
+            </select>
+        </div>
+        <button type='submit' className='btn btn-warning w-100 mb-2'>Submit</button>
+        {error? <small className='text-danger' >{error}</small>:""}
+    </form>
+   </div>
+  )
+}
+
+export default MainForm
